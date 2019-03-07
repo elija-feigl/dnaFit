16660bfb3c0f66aaabf438aab90bddf86e7015fd
elija-feigl <elija.feigl@gmail.com>
2019-03-07 15:11:46 +0100

    init

---
+++ wc_pair-and-mdanalysis_universe.py
@@ -0,0 +1,14 @@
+import MDAnalysis as mda
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+from scipy.optimize import curve_fit
+import sys
+
+import matplotlib.mlab as mlab
+import numpy as np
+import MDAnalysis.analysis.distances
+import MDAnalysis.analysis.nuclinfo
+from MDAnalysis.lib import mdamath
+from math import sqrt
+import pickle
+
