a2c003b7833e24ec7a5cd750b987b7954e155f01
elija-feigl <elija.feigl@gmail.com>
2019-06-13 13:42:29 +0200

    setup

--- segmentation.py
+++ segmentation.py
@@ -3,9 +3,11 @@ import mrcfile
 import numpy as np
 import MDAnalysis as mda
 import sys
+import pickle
 import os
+import ipdb
 
-def mrc_ccc(atoms_selection, path, mrc, context=2, clipping=0.):
+def mrc_segment(atoms_selection, path_in, path_out, context=2, clipping=0.):
 
     if not len(atoms_selection):
         print("EXIT - no atoms in this selection")
@@ -15,7 +17,7 @@ def mrc_ccc(atoms_selection, path, mrc, context=2, clipping=0.):
     u.trajectory[-1]
 
     print(mrcfile)
-    with mrcfile.open(mrc + ".mrc", mode='r+') as mrc:
+    with mrcfile.open(path_in + ".mrc", mode='r+') as mrc:
         m_o = np.array(mrc.header["origin"])
         m_origin = np.array([m_o["x"], m_o["y"], m_o["z"]])
         m_c = np.array(mrc.header["cella"])
@@ -54,7 +56,7 @@ def mrc_ccc(atoms_selection, path, mrc, context=2, clipping=0.):
     # clipp below threshold
     data_small[data_small < clipping] = 0.
 
-    with mrcfile.new(path + "-masked.mrc", overwrite=True) as mrc_out:
+    with mrcfile.new(path_out + ".mrc", overwrite=True) as mrc_out:
         mrc_out.set_data(data_small)
         mrc_out._set_voxel_size(*(cell/grid))
         mrc_out.header["origin"] = tuple(origin)
@@ -67,7 +69,7 @@ def print_usage():
     initializes MDAnalysis universe and compoutes watson scric base pairs. they are returned as to dictionaries. this process is repeated for each Hbond-deviation criterion
     subsequently universe and dicts are stored into a pickle. each devia, tion criterion is stored in one pickle
 
-    usage: projectname designname folder mrcname [context = 2]  ... 
+    usage: designname [context = 2]  ... 
 
     return: creates a pickle for each deviation: the pickle contains: (top, trj), wc_pairs, wc_index_pairs
             the tuple contains the absolute path of the files md-files (universe cannot be pickled), second and third are the two dictionaries
@@ -75,44 +77,74 @@ def print_usage():
 
 
 def proc_input():
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 1:
         print_usage()
-
-    # if isinstance(sys.argv[1], str):
-    project = sys.argv[1]
     # if isinstance(sys.argv[2], str):
-    name = sys.argv[2]
+    name = sys.argv[1]
     cwd = os.getcwd()
-    mrc = sys.argv[4]
-    folder = sys.argv[3]
+    path = cwd + "/"
 
-    path = cwd + "/" + project + "/" + name
-    top = cwd + "/" + project + "/" + name + ".psf"
-    trj = cwd + "/" + project + "/" + folder + "/" + name + ".dcd"
-    mrcname = cwd + "/" + project + "/" + name + mrc
-
-    if len(sys.argv) == 5:
+    if len(sys.argv) == 2:
         context = 2
     else:
-        context = int(sys.argv[5])
+        context = int(sys.argv[2])
 
-    return top, trj, path, context, mrcname
+    return path, name, context
 
 
 def main():
 
-    top, trj, path, context, mrcfile = proc_input()
-    print("read ", top, trj, context)
-    print("output to ", path)
+    path_in, name, context = proc_input()
+    print("output to ", path_in)
+
+    path_analysis = path_in + "/analysis/"
+
+    input_pickle = ["dict_bp", "dict_idid",
+                    "dict_hpid", "dict_coid", "universe"]
+    input_linkage = {}
+    for pickle_name in input_pickle:
+        input_linkage[pickle_name] = pickle.load(
+            open(path_analysis + name + "__" + pickle_name + ".p", "rb"))
 
     # initialize universe and select final frame
-    u = mda.Universe(top, trj)
+    u = mda.Universe(*input_linkage["universe"])
     u.trajectory[-1]
 
+    #full mapp
+    mrc_segment(u.atoms, path_in + name, path_analysis + name + "-all", context=context)
+
+    #crossovers
+    path_out = path_analysis + "/seg-co/"
+    try:     
+        os.mkdir(path_out)
+    except  FileExistsError:
+        pass
+    intig = 0
+    for id_design, co in input_linkage["dict_coid"].items():
+        co_set = set()
+        co_context = 6
+        id_co_design = co["co"]
+
+        co_set.add(input_linkage["dict_idid"][id_design])
+        co_set.add(input_linkage["dict_idid"][id_co_design])
+        co_set.add(input_linkage["dict_bp"][   input_linkage["dict_idid"][id_design]])
+        co_set.add(input_linkage["dict_bp"][   input_linkage["dict_idid"][id_co_design]])
+
+        co_set_fit_add = set()
+        for co in co_set: #TODO: -high- better neighbor search
+            for i in range(-co_context,co_context):
+                co_set_fit_add.add(co+i)
+
+        co_set_fit = co_set | co_set_fit_add
+
+
+        #ipdb.set_trace()
+        atoms_select = mda.AtomGroup([], u)
+        for base_id in co_set_fit:
+            atoms_select += u.residues[base_id].atoms
+        mrc_segment(atoms_select, path_in + name, path_out + name + "-co" +str(intig), context=context)
+        intig += 1
     
-
-    mrc_ccc(u.atoms, path, mrcfile, context=context)
-
 
 if __name__ == "__main__":
     main()
