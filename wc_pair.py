72051a41ae40fd7855b5b94b309cccf06e39ff5a
elija-feigl <elija.feigl@gmail.com>
2019-07-15 13:05:54 +0200

    m

--- wc_pair.py
+++ wc_pair.py
@@ -667,13 +667,14 @@ def main():
         
         #perform analyis
         print("eval_wc", name)
-        bDNA.eval_wc()
+        #bDNA.eval_wc() #TODO: -high uncomment
         print("eval_distances", name)
-        bDNA.eval_distances()
+        #bDNA.eval_distances()
         print("eval_dh", name)
-        bDNA.eval_dh()
+        #bDNA.eval_dh()
         print("eval_co_angles", name)
         bDNA.eval_co_angles()
+        ipdb.set_trace()
         properties.append(bDNA)
         print("write pdbs", name)
         props_tuple = [(bDNA.wc_geometry,"wc_geometry"), (bDNA.wc_quality,"wc_quality"), 
