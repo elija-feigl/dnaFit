c31ade515795a6456f9b7cdf872fda59577018b6
elija-feigl <elija.feigl@gmail.com>
2019-08-22 11:57:48 +0200

    documentaion and format

--- bpLinker.py
+++ bpLinker.py
@@ -9,30 +9,9 @@ import pickle
 from nanodesign.converters import Converter
 from operator import attrgetter
 
-# TODO: -mid- DOC
-"""My numpydoc description of a kind
-         of very exhautive numpydoc format docstring.
-
-         Parameters
-         ----------
-         first : array_like
-             the 1st param name `first`
-         second :
-             the 2nd param
-         third : {'value', 'other'}, optional
-             the 3rd param, by default 'value'
-
-         Returns
-         -------
-         string
-             a value in a string
-
-         Raises
-         ------
-         KeyError
-             when a key error
-         OtherError
-             when an other error"""
+DICTS = ["dict_bp", "dict_idid", "dict_hpid", "dict_color",
+         "dict_coid", "dict_nicks", "list_skips"]
+# TODO: -low move
 
 
 class Linker(object):
@@ -43,11 +22,14 @@ class Linker(object):
         self.path = path
         self.fit = Fit(self.path,)
         self.design = Design(self.path)
-        self.d_Fbp, self.d_DidFid, self.d_DhpsDid = None, None, None
+        self.d_Fbp = None
+        self.d_DidFid = None
+        self.d_DhpsDid = None
         self.d_Fco = None
         self.l_Dskips = self._get_skips()
         self.d_Fnicks = None
         self.d_FidSeq = self._get_sequence()
+        # TODO: d_DhpsFid
 
     def _get_sequence(self):
         d_FidSeq = {}
@@ -55,13 +37,24 @@ class Linker(object):
             d_FidSeq[res.resindex] = res.resname[0]
         return d_FidSeq
 
+    def _is_del(self, base):
+        return base.num_deletions != 0
+
     def _get_skips(self):
+        """ collect position of skips in design
+        -------
+            Returns
+            -------
+            list l_Dskips
+                (base.h, base.p, base.is_scaf) of all skips
+        """
         design_allbases = [
             base for strand in self.design.strands for base in strand.tour]
         l_Dskips = []
         for base in design_allbases:
-            if base.num_deletions != 0:
-                l_Dskips.append((base.h, base.p, base.is_scaf))
+            if self._is_del(base):
+                hp_s = (base.h, base.p, base.is_scaf)
+                l_Dskips.append(hp_s)
 
         return l_Dskips
 
@@ -78,24 +71,22 @@ class Linker(object):
                 -> design-id (int)
         """
 
-        # get all possible idx and positions for a scaffold base
         D_ids = [base.id for base in self.design.scaffold]
         D_hp = [(base.h, base.p, True) for base in self.design.scaffold]
 
-        F_idscaffold = []  # get F_id within scaffold
+        F_id_local = []
         for base in self.design.scaffold:
             indx = D_ids.index(base.id)
-            F_idscaffold.append(indx)
+            F_id_local.append(indx)
 
-        # get F_id global
-        F_ids = self.fit.scaffold.residues[F_idscaffold].resindices
+        F_id_global = self.fit.scaffold.residues[F_id_local].resindices
 
-        d_DidFid = dict(zip(D_ids, F_ids))
+        d_DidFid = dict(zip(D_ids, F_id_global))
         d_DhpsDid = dict(zip(D_hp, D_ids))
         return d_DidFid, d_DhpsDid
 
     def _link_staples(self):
-        """ loop over all staples, then perform the same procedure as scaffold
+        """same procedure as scaffold for each
         -------
          Returns
             -------
@@ -107,29 +98,27 @@ class Linker(object):
             dict d_color
                 fit-segment-id (int) -> color (hex?)
         """
+        def get_resid(segindex, resindex_local):
+            return self.fit.staples[segindex].residues[resindex_local].resindex
+
         d_DidFid = {}
         d_DhpsDid = {}
         d_color = {}
 
         for i, staple in enumerate(self.design.staples):
-            indx_segment = self.design.s_dict[i]
+            seg_id = self.design.s_dict[i]
 
-            # get all possible ids and positions for a base in specific staple
             D_ids = [base.id for base in staple]
             D_hp = [(base.h, base.p, False) for base in staple]
 
-            indx_list = [D_ids.index(base.id)
-                         for base in staple]  # get F_id within staple
-            F_ids = [
-                self.fit.staples[indx_segment].residues[j].resindex for j
-                in indx_list]  # get F_id global
+            F_id_local = [D_ids.index(base.id)for base in staple]
+            F_id_global = [get_resid(seg_id, resid) for resid in F_id_local]
 
-            # get color
             color = self.design.design.strands[staple[0].strand].icolor
-            segidxforcolor = self.fit.staples[indx_segment].segindex
+            segidxforcolor = self.fit.staples[seg_id].segindex
             d_color[segidxforcolor] = color
 
-            d_DidFid_add = dict(zip(D_ids, F_ids))
+            d_DidFid_add = dict(zip(D_ids, F_id_global))
             d_DhpsDid_add = dict(zip(D_hp, D_ids))
             d_DidFid = {**d_DidFid, **d_DidFid_add}
             d_DhpsDid = {**d_DhpsDid, **d_DhpsDid_add}
@@ -148,7 +137,9 @@ class Linker(object):
         d_Fbp = {}
         for base in self.design.scaffold:
             if base.across is not None:
-                d_Fbp[self.d_DidFid[base.id]] = self.d_DidFid[base.across.id]
+                base_Fid = self.d_DidFid[base.id]
+                across_Fid = self.d_DidFid[base.across.id]
+                d_Fbp[base_Fid] = across_Fid
 
         return d_Fbp
 
@@ -178,8 +169,12 @@ class Linker(object):
         self.d_DhpsDid = {**d_DhpsDid_sc, **d_DhpsDid_st}
         self.d_Fbp = self._link_bp()
 
-        return (self.d_Fbp, self.d_DidFid, self.d_DhpsDid, self.d_color,
-                self.l_Dskips)
+        return (self.d_Fbp,
+                self.d_DidFid,
+                self.d_DhpsDid,
+                self.d_color,
+                self.l_Dskips,
+                )
 
     def identify_crossover(self):
         """ for every base id that is involved in a crossover
@@ -188,9 +183,17 @@ class Linker(object):
          Returns
             -------
             dict self.d_Fco
-                fit-id (int) -> TODO
+                fit-id (int) ->
+                "co_index": co_index (int), "co": co_id (int),
+                "leg": leg_id (int),
+                "is_scaffold": is_scaf (bool),
+                "position": (h, p, is_scaf) /(int,int, bool),
+                if end, single, dpouble
+                    "type": ("end", None)
+                    "type": ("single", single, single_leg) /(str, int, int)
+                    "type": ("double", double) /(str, int)
         """
-        def add_co_type(dict_co):
+        def add_co_type(dict_co):  # TODO: -midclean-
             for value in dict_co.values():
                 h, p, is_scaf = value["position"]
                 is_double, is_single, is_end = False, False, False
@@ -218,7 +221,7 @@ class Linker(object):
                         for direct in ["up", "down"]:
                             neigh = get_next(value["base"], direct)
                             if neigh is not None:
-                                while is_ins_del(neigh):
+                                while self._is_del(neigh):
                                     n_new = get_next(neigh, direct)
                                     if n_new is None:
                                         break
@@ -245,20 +248,19 @@ class Linker(object):
             """determine leg base (def: 2 bases away)"""
             l = base.down.p if direct == "up" else base.up.p
             i = (l - base.p) * 2.
-            if (base.h, base.p+i, base.is_scaf) in self.l_Dskips:
+            hp_s = (base.h, base.p+i, base.is_scaf)
+            while hp_s in self.l_Dskips:
                 i = i + np.sign(i)
-            return self.d_DidFid[
-                self.d_DhpsDid[(base.h, base.p+i, base.is_scaf)]]
-
-        def is_ins_del(base):
-            return (base.num_deletions != 0 or base.num_insertions != 0)
+                hp_s = (base.h, base.p+i, base.is_scaf)
+            leg_Did = self.d_DhpsDid[hp_s]
+            return self.d_DidFid[leg_Did]
 
         def get_next(base, direct):
             return (base.up if direct == "up" else base.down)
 
         design_allbases = self.design.scaffold.copy()
-        design_allbases.extend(
-            [base for staple in self.design.staples for base in staple])
+        staples = [base for staple in self.design.staples for base in staple]
+        design_allbases.extend(staples)
 
         dict_co = {}
         co_running_index = 0
@@ -268,7 +270,7 @@ class Linker(object):
             for direct in ["up", "down"]:
                 neighbor = get_next(design_base, direct)
                 if neighbor is not None:
-                    while is_ins_del(neighbor):
+                    while self._is_del(neighbor):
                         n_new = get_next(neighbor, direct)
                         if n_new is None:
                             break
@@ -289,9 +291,13 @@ class Linker(object):
                             co_index = co_running_index
 
                         dict_co[base_Fid] = {
-                            "co_index": co_index, "co": co_id, "leg": leg_id,
+                            "co_index": co_index,
+                            "co": co_id,
+                            "leg": leg_id,
                             "is_scaffold": design_base.is_scaf,
-                            "position": position, "base": design_base}
+                            "position": position,
+                            "base": design_base,
+                        }
 
         dict_co = add_co_type(dict_co)
 
@@ -299,29 +305,33 @@ class Linker(object):
         return self.d_Fco
 
     def identify_nicks(self):
+        """ for every nick, provide id of base accross nick
+        -------
+         Returns
+            -------
+            dict self.d_Fnicks
+                fit-id (int) -> fit_id (int)
+        """
+        def is_nick(candidate, base):
+            is_onhelix = (candidate.h == base.h)
+            is_neighbor = (abs(base.p - candidate.p) <= 2)  # skip = 2
+            is_base = (candidate is not base)
+            return is_onhelix and is_neighbor and not is_base
 
         d_Fnicks = {}
-        # uses skip-cleaned staples
         staple_end_bases = []
         for staple in self.design.staples:
             staple_end_bases.extend((staple[0], staple[-1]))
 
         for base in staple_end_bases:
             for candidate in staple_end_bases:
-                # if skip => 2 else => 1, 0 impossible, abs > 0
-                if (base.h == candidate.h and
-                        abs(base.p - candidate.p) <= 2 and
-                        base is not candidate):
-                    d_Fnicks[self.d_DidFid[base.id]
-                             ] = self.d_DidFid[candidate.id]
+                if is_nick(candidate, base):
+                    base_Fid = self.d_DidFid[base.id]
+                    nick_Fid = self.d_DidFid[candidate.id]
+                    d_Fnicks[base_Fid] = nick_Fid
 
         self.d_Fnicks = d_Fnicks
         return self.d_Fnicks
-
-    def _idx_incl(self, idx):
-        """ include scaffold from idcount
-        """
-        return idx + 1 if idx >= self.design.excl else idx
 
 
 class Fit(object):
@@ -332,12 +342,14 @@ class Fit(object):
         self.scaffold, self.staples = self._split_strands()
 
     def _get_universe(self):
+        # TODO: -mid- if no dcd, try invoke vmd animate write dcd from pdb
         top = self.path + ".psf"
         trj = self.path + ".dcd"
         u = mda.Universe(top, trj)
         return u
 
     def _split_strands(self):
+        # TODO: -low- multiscaffold
         strands = self.u.segments
         scaffold = max(strands, key=attrgetter('residues.n_residues'))
         staples = [strand for strand in strands if len(
@@ -352,31 +364,36 @@ class Design(object):
         self.design = self._get_design()
         self.strands = self.design.strands
         self.scaffold = self._clean_scaffold(self.strands)
-        # TODO: -low- multiscaffold
         self.excl = self.scaffold[0].strand
         self.staples = self._clean_staple(self.strands)
         self.h_dict = self._create_helix_order()
         self.s_dict = self._create_staple_order()
 
+    def _is_del(self, base):
+        return base.num_deletions != 0
+
     def _clean_scaffold(self, strands):
-        scaffold = [strand.tour for strand in strands if strand.is_scaffold][0]
-        scaffold_clean = [
-            base for base in scaffold if base.num_deletions == 0 and
-            base.num_insertions == 0]
+        # TODO: -low- multiscaffold
+        # TODO: -low- insertions
+        scaffold = [s.tour for s in strands if s.is_scaffold][0]
+        scaffold_clean = [b for b in scaffold if not self._is_del(b)]
         return scaffold_clean
 
     def _clean_staple(self, strands):
-        staples = [strand.tour for strand in strands if not strand.is_scaffold]
+        # TODO: -low- insertions
+        staples = [s.tour for s in strands if not s.is_scaffold]
         staples_clean = []
-        for stra in staples:
-            staples_clean.append(
-                [base for base in stra if base.num_deletions == 0 and
-                 base.num_insertions == 0])
+        for s in staples:
+            tour = [b for b in s if not self._is_del(b)]
+            staples_clean.append(tour)
         return staples_clean
 
     def _get_design(self):
         file_name = self.path + ".json"
-        seq_file = self.path + ".seq"
+        try:
+            seq_file = self.path + ".seq"
+        except FileNotFoundError:
+            seq_file = None
         seq_name = None
 
         converter = Converter()
@@ -384,20 +401,27 @@ class Design(object):
         return converter.dna_structure
 
     def _create_helix_order(self):
-        """ helices are not processed in  same order as they are listed by idx
+        """ helices are not listed in the json in same order as they are listed
+            by helix-index. NOTE: enrgMD is helix reorder sensitive.
+        -------
+            Returns
+            -------
+            dict h_dict
+                (int) -> (int)
         """
         helices_dict = self.design.structure_helices_map
         h_dict = {i: h.load_order for (i, h) in helices_dict.items()}
-
         return h_dict
 
     def _create_staple_order(self):
-        """ exchange staple id with load_id
-            map design-staple-order to universe-staple-order
-            staple order is not the same for ergy-server and nd
-            enrg/MDanalysis: sort left to right top to bottom; only counting
-            3' ends nanodesigns/auto: sort left to right top to bottom;
-            counting every staple piece
+        """ enrgMD and nanodesign number staples differently.
+            enrgMD: first occurence of staple sorted by h, p
+            nanodesign: 5' end of staple sorted by h, p
+        -------
+            Returns
+            -------
+            dict s_dict
+                (int) -> (int)
         """
         list_hp = [(self.h_dict[s[0].h], s[0].p) for s in self.staples]
         list_hp_s = sorted(list_hp, key=lambda x: (x[0], x[1]))
@@ -410,17 +434,16 @@ class Design(object):
 
 def print_usage():
     print("""
-    initializes MDAnalysis universe and compoutes watson scric base pairs.
-    they are returned as to dictionaries. this process is repeated for each
-    Hbond-deviation criterion subsequently universe and dicts are stored into a
-    pickle. each deviation criterion is stored in one pickle
+    initializes MDAnalysis universe and creates dictionaries that link bse id
+    in design and fit to design position, wc-partner, nicks, crossovers and
+    staple color.
 
     usage: projectname designname
 
-    return: creates a pickle for each deviation: the pickle contains:
-            (top, trj), wc_pairs, wc_index_pairs
-            the tuple contains the absolute path of the files md-files
-            (universe cannot be pickled), 2nd and 3rd are the two dictionaries
+    return: creates the following pickles:
+        ["dict_bp", "dict_idid", "dict_hpid", "dict_color", "dict_coid",
+        "dict_nicks", "list_skips"]
+        NOTE: (mda-universe cannot be pickled)
         """)
 
 
@@ -448,8 +471,7 @@ def main():
     dict_bp, dict_idid, dict_hpid, dict_color, list_skips = linker.link()
     dict_coid = linker.identify_crossover()
     dict_nicks = linker.identify_nicks()
-    for dict_name in ["dict_bp", "dict_idid", "dict_hpid", "dict_color",
-                      "dict_coid", "dict_nicks", "list_skips"]:
+    for dict_name in DICTS:
         pickle.dump(eval(dict_name), open(
             path + "__" + dict_name + ".p", "wb"))
 
