a63f33acacecc526ff4ea4ca7d723ce1728af8d9
elija-feigl <elija.feigl@gmail.com>
2019-10-28 15:43:53 +0100

    add segemntation specfic attributes

--- project.py
+++ project.py
@@ -14,3 +14,8 @@ class Project(object):
     dev: float = attr.ib(default=0.)
     ENmodify: bool = attr.ib(default=False)
     EN: str = attr.ib(default="11111110")
+    context: int = attr.ib(default=5)
+    range: int = attr.ib(default=10)
+    halfmap: bool = attr.ib(default=False)
+    localres: bool = attr.ib(default=False)
+    star: bool = attr.ib(default=False)
