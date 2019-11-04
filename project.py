2a3d4774cf5e289657ad1b5331413c718712e5ed
elija-feigl <elija.feigl@gmail.com>
2019-11-04 13:12:16 +0100

    d

--- project.py
+++ project.py
@@ -9,11 +9,13 @@ class Project(object):
     input: Path = attr.ib()
     output: Path = attr.ib()
     name: str = attr.ib()
-    # specific
+    # specific FitAnalysis
     frames: int = attr.ib(default=1)
     dev: float = attr.ib(default=0.)
+    # specific FitLinker
     ENmodify: bool = attr.ib(default=False)
     EN: str = attr.ib(default="11111110")
+    # specific segmentation
     context: int = attr.ib(default=5)
     range: int = attr.ib(default=10)
     halfmap: bool = attr.ib(default=False)
